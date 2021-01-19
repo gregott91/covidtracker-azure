e2362257e937b710bbb320864136f8072282f971
gregott91 <gregoryott2345@gmail.com>
2021-01-19 00:03:21 -0600

    password logging

--- CovidTracker.Function/Logic/StaticSiteUpdater.cs
+++ CovidTracker.Function/Logic/StaticSiteUpdater.cs
@@ -41,6 +41,8 @@ namespace CovidTracker.Function.Logic
             string directoryPath = _fileSystemClient.CreateRandomDirectory(path);
             logger.LogInfo($"Working directory: {directoryPath}");
 
+            logger.LogError("TODOGREG " + gitConfig.Password.Length);
+
             try
             {
                 string file = await _packageCoordinator.DownloadPackageAsync(artifactConfig, directoryPath, logger);
